c0ca01f3a1bc8707acbfa0ebcef9e737ac97c183
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-03-24 13:16:08 +0430

    Update Exercise 3.py

--- 1st Series of Exercises/Answers to Exercises/2. Strings Exercises/Exercise 3.py
+++ 1st Series of Exercises/Answers to Exercises/2. Strings Exercises/Exercise 3.py
@@ -12,10 +12,10 @@ print("The number of sentences in the text is:", nofsentences)
 nofvowels= the_text.count("a")+ the_text.count("A")+ the_text.count("e")+ the_text.count("E")+ the_text.count("i")+ the_text.count("I")+ the_text.count("o")+ the_text.count("O")+ the_text.count("u")+ the_text.count("U")
 print("The number of vowels in your text is:", nofvowels)
 # or we can use:
-##count = 0
-##for i in the_text:
-##    if i == "a" or i == "A" or i == "e" or i == "E" or i == "o" or i == "O" or i== "i" or i== "I" or i == "u" or i == "U":
-##        count = count + 1
+#count = 0
+#for i in the_text:
+#    if i == "a" or i == "A" or i == "e" or i == "E" or i == "o" or i == "O" or i== "i" or i== "I" or i == "u" or i == "U":
+#        count = count + 1
 print("Please enter a word for finding:")
 the_word= input()
 result= the_text.find(the_word)
