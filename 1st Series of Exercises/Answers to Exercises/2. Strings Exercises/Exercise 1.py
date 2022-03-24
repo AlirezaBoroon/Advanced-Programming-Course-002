b3a0864f14c4000d6c6bd831627842a1a790c2bd
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-03-24 12:50:14 +0430

    Create Exercise 1.py

---
+++ 1st Series of Exercises/Answers to Exercises/2. Strings Exercises/Exercise 1.py
@@ -0,0 +1,6 @@
+print("Please enter a word with at least 2 characters:")
+word= input()
+pw= word[0]+ word[1]+ word[-2]
+print(pw)
+## dar inja moshahedeh mishavad ke kalameye daraye tanha 1 harf nemitavanad harfe dovvom va yeki mandeh be akhar dashte bashad
+## pas baraye tak cahracter ha karbordi nadarad.
