71d79acc2da91d95eb19ee15f7c2a0521a232f93
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-03-24 12:48:16 +0430

    Create main.py

---
+++ 1st Series of Exercises/Answers to Exercises/1. Input Exercises/main.py
@@ -0,0 +1,6 @@
+print("Please enter a number:")
+number= input()
+number= int(number)
+print(number, "is a type of", type(number))
+## dar tabdile bar'aks, ma darvaghe adadi ra ke vared mikonim be soorate pishfarz, string type mibashad, va agar be int
+## tabdil shavad va dobare be string, kare tekrari anjam shodeh ast.
