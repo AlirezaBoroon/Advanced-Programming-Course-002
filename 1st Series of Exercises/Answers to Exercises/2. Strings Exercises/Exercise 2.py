3af9ec73f850fa14650b5161c20f8c79a516ad0d
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-03-24 12:51:36 +0430

    Create Exercise 2.py

---
+++ 1st Series of Exercises/Answers to Exercises/2. Strings Exercises/Exercise 2.py
@@ -0,0 +1,8 @@
+print("Please enter 3 words:")
+fword= input()
+sword= input()
+tword= input()
+## Dar Python shomaresh az 0 mibashad.
+final_word= fword[len(fword)//2]+ sword[len(sword)//2]+ tword[len(tword)//2]
+## Dar taghsim haye bala chon kaf hesab mishavd va ma bayad ba 1 jam' konim, vali niazi nist, be dalile shomaresh az 0.
+print("The word's made:", final_word)
